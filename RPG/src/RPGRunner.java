44e02594214ccb40468e8e1f04ef7c0911d279d2
YuyiZhao <mark.yuyi.zhao@gmail.com>
2019-04-27 15:36:52 -0700

    random stuff

--- RPG/src/RPGRunner.java
+++ RPG/src/RPGRunner.java
@@ -1,30 +1,20 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
 
-import javax.swing.AbstractAction;
-import javax.swing.ActionMap;
-import javax.swing.InputMap;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.KeyStroke;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
 import javax.swing.Timer;
 
 public class RPGRunner implements KeyListener {
 
 	private JPanel panel;
 	private Timer timer;
+	//currently 200 times per second, i think? 1000 ticks/5 
 	private static final int REFRESH_RATE = 5;
+	int ticks = 0;
 	public static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-	public static final int WIDTH = (int)(screenSize.getWidth()*3/4), HEIGHT = (int)(screenSize.getHeight()*3/4);
+	public static final int WIDTH = (int) (screenSize.getWidth() * 3 / 4),
+			HEIGHT = (int) (screenSize.getHeight() * 3 / 4);
 	Player player;
 	Enemy e;
 	private ArrayList<String> keys = new ArrayList<String>();
@@ -36,30 +26,30 @@ public class RPGRunner implements KeyListener {
 	private void init() {
 		JFrame frame = new JFrame("Role Playing Game");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		player = new Player(50,50,50,50);
-		e = new Enemy(100,100,100,100);
+		player = new Player(50, 50, 50, 50);
+		e = new Enemy(100, 100, 100, 100);
 		panel = new JPanel() {
 
 			@Override
 			public void paintComponent(Graphics g) {
 				super.paintComponent(g);
-				
+
 				e.draw(g);
 				player.draw(g);
 			}
 		};
-		
+
 		// frame doesn't get minimized
 		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		// frame gets placed a little way from top and left side
-		frame.setLocation(WIDTH/10, HEIGHT/10);
+		frame.setLocation(WIDTH / 10, HEIGHT / 10);
 		// background
 		panel.setBackground(new Color(250, 250, 250));
 		panel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent me) {
-				//clickedAt(me);
-				//to-do
+				// clickedAt(me);
+				// to-do
 
 			}
 		});
@@ -69,20 +59,22 @@ public class RPGRunner implements KeyListener {
 		frame.setVisible(true);
 		frame.addKeyListener(this);
 
-		// this timer controls the actions in the game and then repaints after each update to data
+		// this timer controls the actions in the game and then repaints after each
+		// update to data
 		timer = new Timer(REFRESH_RATE, new ActionListener() {
+			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				panel.repaint();
-				direction();
+				controls();
+				ticks++;
 			}
 
-			
 		});
 		timer.start();
 	}
 
-	private void direction() {
+	private void controls() {
 		if (keys.contains("w")) {
 			player.moveY(-1.7);
 		}
@@ -95,28 +87,30 @@ public class RPGRunner implements KeyListener {
 		if (keys.contains("d")) {
 			player.moveX(1.7);
 		}
-		
+
+		if (keys.contains("j")) {
+			player.attack(keys,ticks);
+		}
+
 	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if(!keys.contains(""+e.getKeyChar()))
-			keys.add(""+e.getKeyChar());
-	
+		if (!keys.contains("" + e.getKeyChar()))
+			keys.add("" + e.getKeyChar());
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		if(keys.contains(""+e.getKeyChar()))
-			keys.remove(""+e.getKeyChar());
-	
-		
+		if (keys.contains("" + e.getKeyChar()))
+			keys.remove("" + e.getKeyChar());
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		
-	}
 
-	
+	}
 
 }
