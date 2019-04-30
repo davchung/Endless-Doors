f5a9ad233014027e5566e96d0e514d8a6a4476d7
Ethan Yeung <ethan2717@gmail.com>
2019-04-30 11:32:47 -0700

    change rock.png image

--- RPG/src/Chest.java
+++ RPG/src/Chest.java
@@ -2,7 +2,8 @@ import java.awt.*;
 
 public class Chest extends GameObject{
 	
-	double amountOfGold;
+	private double amountOfGold;
+	
 	public Chest(double x, double y, double w, double h, double gold) {
 		super(x, y, w, h, "chest.png");
 		amountOfGold = gold;
