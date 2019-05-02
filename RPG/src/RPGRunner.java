66542395855fd561c34f8ee535ba03f2ce892e1e
davchung <davidchung7186@gmail.com>
2019-05-01 18:47:07 -0700

    added comment

--- RPG/src/RPGRunner.java
+++ RPG/src/RPGRunner.java
@@ -133,7 +133,7 @@ public class RPGRunner implements KeyListener {
 				Attack a = new Attack((int) player.getLocX() + 25, (int) player.getLocY() + 25, right, down, ticks);
 			}
 		}
-		a.update(Math.abs(down) + Math.abs(right), ticks);
+		a.update(Math.abs(down) + Math.abs(right), ticks); //IF YOU WANT TO RUN SOMETHING COMMENT THIS LINE OUT
 
 		for (GameObject e : objects) {
 			if (player.equals(e))
