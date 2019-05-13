7348c3e5c01cb678d7df54bbcfc21e0ec299dab0
j-yeeezy <45783522+j-yeeezy@users.noreply.github.com>
2019-05-12 23:04:05 -0700

    Revised errors with health and objects

--- RPG/src/Environment.java
+++ RPG/src/Environment.java
@@ -4,11 +4,11 @@ public class Environment extends GameObject{
 	
 	// constructor #1 for Environment
 	public Environment(double x, double y, double w, double h, boolean through, String s) {
-		super(x, y, w, h, through, 0, s); // uses GameObject's constructor #1
+		super(x, y, w, h, through, Integer.MAX_VALUE, s); // uses GameObject's constructor #1
 	}
 	
 	// constructor #2 for Environment
 	public Environment(double x, double y, double w, double h, boolean through, BufferedImage i) {
-		super(x, y, w, h, through, 0, i); // uses GameObject's constructor #2
+		super(x, y, w, h, through, Integer.MAX_VALUE, i); // uses GameObject's constructor #2
 	}
 }
