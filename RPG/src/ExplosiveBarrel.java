a3479c76dc604f0aa1a9db30c362cc9e9ffa0669
YuyiZhao <mark.yuyi.zhao@gmail.com>
2019-05-16 21:35:07 -0700

    Hand-drawn barrels

--- RPG/src/ExplosiveBarrel.java
+++ RPG/src/ExplosiveBarrel.java
@@ -3,7 +3,7 @@ public class ExplosiveBarrel extends GameObject {
 
 
 	public ExplosiveBarrel(double x, double y, int w, int h) {
-		super(x, y, w, h, false, false, 25, "barrel.png");
+		super(x, y, w, h, false, false, 25, "sprites/barrel.png");
 	}
 	
 	public void explode() {
