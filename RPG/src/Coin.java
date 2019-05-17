d5396afad438c2ab3b2907fbc2183633594426d3
YuyiZhao <mark.yuyi.zhao@gmail.com>
2019-05-16 22:04:06 -0700

    Better sized coins

--- RPG/src/Coin.java
+++ RPG/src/Coin.java
@@ -4,7 +4,7 @@ public class Coin extends GameObject {
 	private static Animation spin= new Animation("coin", 4); 
 	
 	public Coin(double x, double y, int gold) {
-		super(x, y, 50, 50, true, true, gold, spin.getFirst());
+		super(x+15, y+15, 20, 20, true, true, gold, spin.getFirst());
 
 	}
 	public void draw(Graphics g) {
