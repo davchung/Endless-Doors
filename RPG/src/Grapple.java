edb1357102f5ca84f10c26f211ca796f8d1327ce
Mark <mark.yuyi.zhao@gmail.com>
2019-05-20 12:19:05 -0700

    Other things

--- RPG/src/Grapple.java
+++ RPG/src/Grapple.java
@@ -1,4 +1,10 @@
 
 public class Grapple extends Attack {
 
+	public Grapple(int x, int y, int width, int height, int pWidth, int pHeight, double right, double down,
+			int velocity, int duration, double e, String s) {
+		super(x, y, width, height, pWidth, pHeight, right, down, velocity, duration, e, s);
+		// TODO Auto-generated constructor stub
+	}
+
 }
