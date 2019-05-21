1be88d483e2369f0361e935b66dba945e7991fd6
YuyiZhao <mark.yuyi.zhao@gmail.com>
2019-05-20 20:12:54 -0700

    Added in arrows. one of them better than the other.

--- RPG/src/RPGGame.java
+++ RPG/src/RPGGame.java
@@ -95,7 +95,6 @@ public class RPGGame implements KeyListener {
 	public void beginGame() {
 		gameLevel = 1;
 		player = new Archer(StartGame.SCREEN_HEIGHT / 2, StartGame.SCREEN_WIDTH / 2);
-		// player = new Knight(100, 100, 50, 50);
 		m = new Map();
 		objects.addAll(m.getWalls());
 		objects.addAll(m.getEObjs());
@@ -124,7 +123,7 @@ public class RPGGame implements KeyListener {
 			@Override
 			public void paintComponent(Graphics g) {
 				super.paintComponent(g);
-				floor.drawFloor(g); //draws a floor...kinda
+				//floor.drawFloor(g); //draws a floor...kinda
 				for (GameObject go : objects) {
 					go.draw(g); // draws all objects
 				}
