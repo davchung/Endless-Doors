343a70da1de0eaf5f8e9cad284d9e573421f66f7
YuyiZhao <mark.yuyi.zhao@gmail.com>
2019-05-26 18:10:24 -0700

    Fixed everything. Somehow.

--- RPG/src/Barrel.java
+++ RPG/src/Barrel.java
@@ -5,13 +5,5 @@ public class Barrel extends GameObject {
 	public Barrel(double x, double y, int w, int h) {
 		super(x, y, w, h, false, false, 10, "sprites/barrel.png");
 	}
-	
-	@Override
-	public void draw(Graphics g) {
-		if (image != null) {
-			g.drawImage(image, (int) locX, (int) locY, WIDTH, HEIGHT, null);
-		}
-
-	}
 
 }
