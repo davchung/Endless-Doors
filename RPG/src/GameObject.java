b11f10ac8bea4b7d77d45ae752e87df6ad914c1d
YuyiZhao <mark.yuyi.zhao@gmail.com>
2019-05-26 17:56:56 -0700

    Dunno how these got here, removed them

--- RPG/src/GameObject.java
+++ RPG/src/GameObject.java
@@ -33,7 +33,6 @@ public abstract class GameObject {
 		invincible = inv;
 		health = startingHealth;
 		maxHealth = startingHealth;
-
 		image = getImage(s);
 		current = new Rectangle((int) locX, (int) locY, (int) WIDTH, (int) HEIGHT);
 		gameID = IDAssigner;
@@ -51,7 +50,6 @@ public abstract class GameObject {
 		invincible = inv;
 		health = startingHealth;
 		maxHealth = startingHealth;
-
 		image = b;
 		current = new Rectangle((int) locX, (int) locY, (int) WIDTH, (int) HEIGHT);
 		gameID = IDAssigner;
